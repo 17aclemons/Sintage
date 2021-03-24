32d103e81da3b62ff07693409788b29eaa7870f0
Andrew Clemons <17aclemons@gmail.com>
2021-03-24 10:41:36 -0600

    button to move to PhotoRecognizer Activity
    Added a example on how to make a button to switch between activities

---
+++ app/src/main/java/com/example/sintage/PhotoRecognizer.kt
@@ -0,0 +1,11 @@
+package com.example.sintage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoRecognizer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_recognizer)
+    }
+}
