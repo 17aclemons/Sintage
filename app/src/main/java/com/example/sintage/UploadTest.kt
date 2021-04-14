f65a689ec89f003f619a9afc6635c9cc71d5f217
Andrew Clemons <17aclemons@gmail.com>
2021-04-14 15:33:17 -0600

    changed minSDK value

---
+++ app/src/main/java/com/example/sintage/UploadTest.kt
@@ -0,0 +1,11 @@
+package com.example.sintage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_test)
+    }
+}
