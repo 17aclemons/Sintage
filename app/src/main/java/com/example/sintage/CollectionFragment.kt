72b25fdac583d9f0afc17fb86da595c031385851
Nikki Wood <woodenikki@gmail.com>
2021-04-18 13:10:35 -0600

    Collections working (But absolutely atrocious)

--- app/src/main/java/com/example/sintage/CollectionFragment.kt
+++ app/src/main/java/com/example/sintage/CollectionFragment.kt
@@ -5,55 +5,87 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CollectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CollectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+lateinit var auth: FirebaseAuth
+lateinit var favoriteWineList : WineList
+lateinit var collectionWineList : WineList
+lateinit var listAdapter : ArrayAdapter<String>
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        //val view = inflater.inflate(R.layout.fragment_collection, container, false)
+        val view = LayoutInflater.from(container?.context).inflate(R.layout.fragment_collection, container, false)
+        var favoritesBtn = view.findViewById<Button>(R.id.favoritesButton) //saved
+        var collectionBtn = view.findViewById<Button>(R.id.collectionButton) //all scanned
+        var list = view.findViewById<ListView>(R.id.listViewer)
+
+
+
+        favoritesBtn.setOnClickListener {
+            listAdapter = ArrayAdapter(container!!.getContext(), android.R.layout.simple_list_item_1, favoriteWineList.list)
+            list.adapter = listAdapter
+
+        }
+
+        collectionBtn.setOnClickListener {
+            listAdapter = ArrayAdapter(container!!.getContext(), android.R.layout.simple_list_item_1, favoriteWineList.list)
+            list.adapter = listAdapter
+        }
+
+        return view
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        collectionWineList = WineList()
+        favoriteWineList = WineList()
+
+        if (auth.currentUser != null) {
+            db.collection("${auth.currentUser?.uid.toString()}_saved").get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            for (document in task.result!!) {
+                                favoriteWineList.add(document.data["name"].toString())
+                            }
+                        }
+
+                    }
+
+            db.collection("${auth.currentUser?.uid.toString()}_scanned").get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            for (document in task.result!!) {
+                                collectionWineList.add(document.data["name"].toString())
+                            }
+                        }
+
+                    }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collection, container, false)
-    }
+    //TODO: Implement count
+    class WineList(){
+        var list = arrayListOf<String>()
+//        var count = arrayListOf<Int>()
+        fun add(wine: String){
+            list.add(wine)
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CollectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CollectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun get(index: Int): String{
+            return list.get(index)
+        }
     }
 }
